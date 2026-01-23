a6907fa068fc61a97187d396b456e8373857aaab
Cira Simion Florin <https://github.com/SimionFlorin>
2026-01-23 18:26:59 +0200

    daily-horoscope

---
+++ daily-horoscope/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HoroscopeFetcher from './HoroscopeFetcher';
+
+function App() {
+  return (
+    <div className="App">
+      <HoroscopeFetcher />
+    </div>
+  );
+}
+
+export default App;
